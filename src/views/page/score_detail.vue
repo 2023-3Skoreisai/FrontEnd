0fe36f3f8d1eb21aefea2761c9d4801729d514a7
Crysta1221 <takuyakojim@outlook.jp>
2023-10-11 18:07:12 +0900

    [+] Add New Components

--- src/views/page/score_detail.vue
+++ src/views/page/score_detail.vue
@@ -1,3 +1,14 @@
 <template>
-  <a>score_detail</a>
+  <div class="flex flex-col items-center">
+    <Icon icon="tabler:alert-circle-filled" color="dodgerblue" class="text-4xl mb-4" height="50" width="50"/>
+    <h1 class="text-2xl">この先はダッシュボード外のページです</h1>
+    <br/>
+    <h2 class="text-md ml-4">「スコア表示」ページは<a href="/score" class="text-blue-700">データベース > スコア表示</a>ページの一般公開版になります。このページを離れるとダッシュボードからはセキュリティ上の理由により自動でログアウトされます。</h2>
+    <div class="mt-8">
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
--- src/views/score.vue
+++ src/views/score.vue
@@ -1,3 +1,144 @@
 <template>
-  <a>score</a>
+  <div class="grid grid-cols-2 gap-4">
+    <div>
+      <va-card stripe stripe-color="warning" class="mt-4">
+        <va-card-title>1st Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">1</div>
+              <div class="text-lg font-bold">User 1</div>
+            </div>
+            <div class="text-lg font-bold">100</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="primary" class="mt-4">
+        <va-card-title>6st Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">6</div>
+              <div class="text-lg font-bold">User 6</div>
+            </div>
+            <div class="text-lg font-bold">40</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="success" class="mt-4">
+        <va-card-title>2nd Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">2</div>
+              <div class="text-lg font-bold">User 2</div>
+            </div>
+            <div class="text-lg font-bold">90</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="primary" class="mt-4">
+        <va-card-title>7th Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">7</div>
+              <div class="text-lg font-bold">User 7</div>
+            </div>
+            <div class="text-lg font-bold">30</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="info" class="mt-4">
+        <va-card-title>3rd Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">3</div>
+              <div class="text-lg font-bold">User 3</div>
+            </div>
+            <div class="text-lg font-bold">80</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="primary" class="mt-4">
+        <va-card-title>8th Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">8</div>
+              <div class="text-lg font-bold">User 8</div>
+            </div>
+            <div class="text-lg font-bold">20</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="primary" class="mt-4">
+        <va-card-title>4th Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">4</div>
+              <div class="text-lg font-bold">User 4</div>
+            </div>
+            <div class="text-lg font-bold">70</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="primary" class="mt-4">
+        <va-card-title>9th Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">9</div>
+              <div class="text-lg font-bold">User 9</div>
+            </div>
+            <div class="text-lg font-bold">10</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="primary" class="mt-4">
+        <va-card-title>5st Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">5</div>
+              <div class="text-lg font-bold">User 5</div>
+            </div>
+            <div class="text-lg font-bold">50</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div>
+      <va-card stripe stripe-color="primary" class="mt-4">
+        <va-card-title>10th Place</va-card-title>
+        <va-card-content>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center">
+              <div class="text-2xl font-bold mr-4">10</div>
+              <div class="text-lg font-bold">User 10</div>
+            </div>
+            <div class="text-lg font-bold">0</div>
+          </div>
+        </va-card-content>
+      </va-card>
+    </div>
+  </div>
 </template>
